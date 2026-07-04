43742ae3f9c15e09dfa7ddcf886929c75783c916
DESKTOP-J0PJURI\EMOLITE <baovq@amfcorp.vn>
2026-07-04 10:47:33 +0700

    update api store

---
+++ src/FoodEmolite.Application/DTOs/StoreFood/GetStoreFoodsRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodEmolite.Application.DTOs.StoreFood
+{
+    public class GetStoreFoodsRequest
+    {
+        public string StoreRefCode { get; set; } = null!;
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public long? StoreFoodCategoryId { get; set; }
+    }
+}
